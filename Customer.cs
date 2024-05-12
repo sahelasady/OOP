8be27a45117e11a661f6393f25c4d9db6e7348fc
Sahel Asadyrajaei <sasadyrajaei@myseneca.ca>
2024-05-12 17:14:22 -0400

    Constructors

---
+++ Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> orders;
+
+        //Default, parameterless constructors
+        public Customer()
+        {
+            //It's always best practice to initialize a list with an empty list to prevent null reference exception
+            orders = new List<Order>();
+        }
+        //We can use this keyword and specify any of the other constructors we would like to pass the control to
+        public Customer(int id) : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -26,8 +26,10 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            var customer = new Customer(1, "John");
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
